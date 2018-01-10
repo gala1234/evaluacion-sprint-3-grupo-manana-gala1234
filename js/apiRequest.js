60685adf1f3c7319596be8255bf498b56bbb3266
gala1234 <calero.vg@gmail.com>
2018-01-10 11:56:42 +0100

    solve JS more reasons other issue

--- js/apiRequest.js
+++ js/apiRequest.js
@@ -7,7 +7,7 @@
 // creo el evento que escribe las 3 nuevas razones
 
 
-var reasonsListHTML = '';
+
 var btnMoreReasons = document.querySelector('.block_main_section-reason-more');
 var section = document.querySelector('.block_main_section-reason-articles');
 
@@ -20,6 +20,7 @@ function moreReasons(){
 	request.addEventListener('load', function(){
 		var response = request.responseText;
 		var responseJSON = JSON.parse(response).reasons;
+		var reasonsListHTML = '';
 		for (var i = 0; i < responseJSON.length; i++) {
 			reasonsListHTML += '<article class="block_main_section-reason-article"><h3 class="Reason-title---mobile">'+ responseJSON[i].title + '</h3><p class="Reason-description---mobile">' + responseJSON[i].description +'</p></article>';
 		}
