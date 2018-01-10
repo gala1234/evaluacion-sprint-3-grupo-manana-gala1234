c361aebfe4fe4e8929d983414bb088c279b1829f
gala1234 <calero.vg@gmail.com>
2018-01-10 11:49:42 +0100

    solve JS more reasons issue

--- js/apiRequest.js
+++ js/apiRequest.js
@@ -5,25 +5,26 @@
 
 
 // creo el evento que escribe las 3 nuevas razones
-var request = new XMLHttpRequest();
-request.open('GET', 'https://three-random-reasons-izwfjpgbqm.now.sh/');
 
 
+var reasonsListHTML = '';
 var btnMoreReasons = document.querySelector('.block_main_section-reason-more');
+var section = document.querySelector('.block_main_section-reason-articles');
 
-btnMoreReasons.addEventListener('click', function(){
-  request.addEventListener('load', moreReasons);
-  request.send();
-})
-
+btnMoreReasons.addEventListener('click', moreReasons);
 
 function moreReasons(){
-      var response = request.responseText;
-      var responseJSON = JSON.parse(response).reasons;
-      var reasonsListHTML = '';
-	    for (var i = 0; i < responseJSON.length; i++) {
-		  reasonsListHTML += '<div class="block_main_section-reason-article"><h3 class="Reason-title---mobile">'+ responseJSON[i].title + '</h3><p class="Reason-description---mobile">' + responseJSON[i].description +'</p></div>';
-	}
-  var section = document.querySelector('.block_main_section-reason-articles');
-  section.innerHTML += reasonsListHTML;
+	var request = new XMLHttpRequest();
+	request.open('GET', 'https://three-random-reasons-izwfjpgbqm.now.sh/');
+
+	request.addEventListener('load', function(){
+		var response = request.responseText;
+		var responseJSON = JSON.parse(response).reasons;
+		for (var i = 0; i < responseJSON.length; i++) {
+			reasonsListHTML += '<article class="block_main_section-reason-article"><h3 class="Reason-title---mobile">'+ responseJSON[i].title + '</h3><p class="Reason-description---mobile">' + responseJSON[i].description +'</p></article>';
+		}
+		section.innerHTML += reasonsListHTML;
+	});
+
+	request.send();
 };
